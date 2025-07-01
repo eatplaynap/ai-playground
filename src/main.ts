41169c1722b83c9008ff486efb601653475064d0
eatplaynap <eatplaynap329@gmail.com>
2025-07-01 17:35:28 +0900

    GoogleGenAIの導入と簡単なサンプルコードの追加
    どうやってJSで環境変数設定するのか忘れたわ

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-// write something here
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "How does AI work?",
+  });
+  console.log(response.text);
+}
+
+await main();
