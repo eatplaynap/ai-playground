f36c18885c9f14f6af8a9ad3465621fdf2295050
eatplaynap <eatplaynap329@gmail.com>
2025-07-02 19:08:09 +0900

    ユーザー入力を取得するためのreadlineプロンプトを追加

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
-import { GoogleGenAI } from '@google/genai'
+import { GoogleGenAI } from "@google/genai";
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 
 const apiKey = process.env.GOOGLE_API_KEY
 const ai = new GoogleGenAI({ apiKey })
 
+
+
+const rl = readline.createInterface({ input, output });
+
+const answer = await rl.question("What do you want to know?");
+
 async function main() {
   const response = await ai.models.generateContent({
-    model: 'gemini-2.5-flash',
-    contents: 'Do you know what tomorrow will bring?',
-  })
-  console.log(response.text)
+    model: "gemini-2.5-flash",
+    contents: answer,
+  });
+  console.log(response.text);
 }
 
 await main()
