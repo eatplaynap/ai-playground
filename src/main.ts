ebf871e3d3384e375ecbf61deb52b541aad1d769
eatplaynap <eatplaynap329@gmail.com>
2025-07-02 17:29:16 +0900

    AI呼び出し内容を詩的にした

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { GoogleGenAI } from "@google/genai";
-import 'dotenv/config'
 
 const apiKey = process.env.GOOGLE_API_KEY;
 const ai = new GoogleGenAI({apiKey});
@@ -7,7 +6,7 @@ const ai = new GoogleGenAI({apiKey});
 async function main() {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "How does AI work?",
+    contents: "Do you know what tomorrow will bring?",
   });
   console.log(response.text);
 }
