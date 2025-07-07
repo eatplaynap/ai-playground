0242ca13575ae522ad9fcadfdfc2707257ac9bf2
eatplaynap <eatplaynap329@gmail.com>
2025-07-07 19:00:48 +0900

    コメントを追加

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { stdin as input, stdout as output } from 'node:process'
 import * as readline from 'node:readline/promises'
+
+// トランスパイル後のdist/text2comedy.jsを見に行く。
 import { text2comedy } from './text2comedy.js'
 
 const rl = readline.createInterface({ input, output })
