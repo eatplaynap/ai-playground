084b53a0e6ae2886bb1e6ba4515e4dbdd5d355c4
eatplaynap <eatplaynap329@gmail.com>
2025-07-10 18:39:11 +0900

    モックが書けないのでとりあえずコメント 型が複雑なので一旦jsで書いてもいいかも

---
+++ test/text2comedy.test.ts
@@ -0,0 +1,7 @@
+import { describe, it } from 'vitest'
+
+describe('text2comedy', () => {
+  it.todo('generateContentStreamが呼び出される', async () => {
+    // GoogleGenAIの関数をモックし、関数が呼ばれたことを検証する
+  })
+})
